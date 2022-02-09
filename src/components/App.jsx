8793687856bdf58ea6f5e42468f5f241ee66989a
okolobaha-me <yevheniy.onyshchenko@gmail.com>
2022-02-09 14:06:17 +0200

    profile added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
+import Profile from './Profile/Profile';
+import StatisticList from './StatisticList/StatisticList';
+import user from './user.json';
+import data from './data.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <StatisticList stats={data} />
     </div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+
+function Profile({ username, tag, location, avatar, stats }) {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={`${avatar}`} alt="User avatar" className="avatar" />
+        <p className="name">{`${username}`}</p>
+        <p className="tag">@{`${tag}`}</p>
+        <p className="location">{`${location}`}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{` ${stats.followers}`}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{` ${stats.views}`}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{` ${stats.likes}`}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Profile;
+
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.objectOf(PropTypes.number),
+  tag: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+};
